c83af79b109eba73933bc237c2c5a8576526723d
Chansu Kim <ckstn0777@naver.com>
2023-01-14 17:59:10 +0900

    feat: CommendCard refactoring

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { HeartFill } from './icons'
+import { HeartFill, Heart } from './icons'
 
 function CommentCard() {
   return (
@@ -26,12 +26,74 @@ function CommentCard() {
         </div>
       </div>
 
-      <div className="flex items-center gap-1">
+      <HeartBox isHeart={true}>5</HeartBox>
+      {/* <div className="flex items-center gap-1">
         <HeartFill className="text-primary" />
         <span className="text-caption1">5</span>
-      </div>
+      </div> */}
     </div>
   )
 }
+
+function UserInfo({
+  username,
+  profile,
+  regDate,
+}: {
+  username: string
+  profile: string
+  regDate: string | Date
+}) {
+  return (
+    <div className="flex justify-start items-center gap-1">
+      <Image
+        src="http://source.unsplash.com/OekcvtMyjG8"
+        alt="article image"
+        width={24}
+        height={24}
+        className="w-7 h-7 rounded-full"
+      />
+      <span className="text-caption1 text-gray5">{username}</span>
+      <span className="text-caption1 text-gray5">{regDate.toString()}</span>
+    </div>
+  )
+}
+
+function Comment({ children }: { children: React.ReactNode }) {
+  return <p className="text-body2">{children}</p>
+}
+
+function AddInfo({
+  isReply = true,
+  isDelete = true,
+  isReport = true,
+}: {
+  isReply: boolean
+  isDelete: boolean
+  isReport: boolean
+}) {
+  return (
+    <div className="flex gap-2 text-caption1 text-gray5">
+      {isReply && <span>답글</span>}
+      {isDelete && <span>삭제</span>}
+      {isReport && <span>신고</span>}
+    </div>
+  )
+}
+
+function HeartBox({ isHeart, children }: { isHeart: boolean; children: React.ReactNode }) {
+  return (
+    <div className="flex items-center gap-1">
+      {isHeart ? <HeartFill className="text-primary" /> : <Heart />}
+
+      <span className="text-caption1">{children}</span>
+    </div>
+  )
+}
+
+CommentCard.UserInfo = UserInfo
+CommentCard.Comment = Comment
+CommentCard.AddInfo = AddInfo
+CommentCard.HeartBox = HeartBox
 
 export default CommentCard
